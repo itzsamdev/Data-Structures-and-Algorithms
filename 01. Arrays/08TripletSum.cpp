4d4b2f56d503631e0a3599ad3eb6719693d655db
Saiyam Jain <jimisierra2@gmail.com>
2022-06-30 19:33:20 +0530

    Triplet Sum

---
+++ 01. Arrays/08TripletSum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity O(N log N)+ N
+vector<int> findTriplet(vector<int> arr, int S)
+{
+    sort(arr.begin(), arr.end());
+    vector<int> ans;
+    for (int i = 0; i < arr.size() - 2; i++)
+    {
+        int curr = arr[i];
+        int target = S - curr;
+        int s = i + 1, e = arr.size() - 1;
+        while (s < e)
+        {
+            int sum = arr[s] + arr[e];
+            if (sum == target)
+            {
+                ans.push_back(arr[s]);
+                ans.push_back(arr[e]);
+                ans.push_back(arr[i]);
+                return ans;
+            }
+            else if (sum < target)
+            {
+                s++;
+            }
+            else
+            {
+                e--;
+            }
+        }
+    }
+    return {-1, -1, -1};
+}
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
+    int S = 18;
+    auto ans = findTriplet(arr, S);
+    for (auto x : ans)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
