738af6e32fc162e14f567293c1e1bb758cacd7ea
Saiyam Jain <jimisierra2@gmail.com>
2022-06-30 19:46:41 +0530

    Merge Sort

---
+++ 02. Sorting/05MergeSort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeSort(int arr[], int n)
+{
+}
+int main()
+{
+    int arr[] = {13, 5, 9, 45, 27, 36, 58, 12};
+    int n = sizeof(arr) / sizeof(int);
+    mergeSort(arr, n);
+    return 0;
+}
