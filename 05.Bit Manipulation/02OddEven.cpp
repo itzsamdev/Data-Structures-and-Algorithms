74f93d69a4a32785fd01000435344cb1a446b79b
Saiyam Jain <jimisierra2@gmail.com>
2022-06-30 11:43:10 +0530

    Check Odd/Even

---
+++ 05.Bit Manipulation/02OddEven.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 121;
+    if (n & 1)
+    {
+        cout << "Odd" << endl;
+    }
+    else
+    {
+        cout << "Even" << endl;
+    }
+    return 0;
+}
