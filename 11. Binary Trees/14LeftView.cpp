4ab74a2b79135c13857babaf46ed6a58ee088191
Saiyam Jain <jimisierra2@gmail.com>
2022-07-11 23:01:23 +0530

    Left View of Binary Tree

---
+++ 11. Binary Trees/14LeftView.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node()
+    {
+        data = 0;
+        left = NULL;
+        right = NULL;
+    }
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int maxLevel = 0;
+
+void solve(Node *root, vector<int> &ans, int level)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (ans.size() == level)
+    {
+        ans.push_back(root->data);
+    }
+    solve(root->left, ans, level + 1);
+    solve(root->right, ans, level + 1);
+}
+// Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+    vector<int> ans;
+    solve(root, ans, 0);
+    return ans;
+}
