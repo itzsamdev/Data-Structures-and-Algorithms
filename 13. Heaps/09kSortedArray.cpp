09b8b326dfc580b46adc005ea99b082e3be98d1e
Saiyam Jain <jimisierra2@gmail.com>
2022-07-14 23:50:35 +0530

    Sort K Sorted Array

--- 13. Heaps/09kSortedArray.cpp
+++ 13. Heaps/09kSortedArray.cpp
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> nearlySorted(int arr[], int num, int K)
+// O(n log K)
+vector<int> nearlySorted(int arr[], int num, int k)
 {
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-    int size = (K == num) ? K : K + 1;
-    priority_queue<int, vector<int>, greater<int>> pq(arr, arr + size);
-
-    int index = 0;
-    for (int i = K + 1; i < num; i++)
+    vector<int> res;
+    for (int i = 0; i <= k; i++)
     {
-        arr[index++] = pq.top();
+        pq.push(arr[i]);
+    }
+    for (int i = k + 1; i < num; i++)
+    {
+        res.push_back(pq.top());
         pq.pop();
         pq.push(arr[i]);
     }
     while (!pq.empty())
     {
-        arr[index++] = pq.top();
+        res.push_back(pq.top());
         pq.pop();
-    }
-    vector<int> res;
-    for (int i = 0; i < num; i++)
-    {
-        res.push_back(arr[i]);
     }
     return res;
 }
