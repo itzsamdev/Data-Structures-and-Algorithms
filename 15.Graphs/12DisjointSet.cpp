b2749c5f877ebb3d44911fe48269ff1d529ffca1
Saiyam Jain <jimisierra2@gmail.com>
2022-07-24 06:10:50 +0530

    Disjoint Set ( Find union by rank & Path Compression )

---
+++ 15.Graphs/12DisjointSet.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int parent[10000];
+int ranks[10000];
+// Making set
+void makeSet(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = i;
+        ranks[i] = 0;
+    }
+}
+// Finding parent
+int findPar(int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+    return parent[node] = findPar(parent[node]);
+}
+// Combine both
+void Union(int x, int y)
+{
+    x = findPar(x);
+    y = findPar(y);
+
+    if (ranks[x] < ranks[y])
+    {
+        parent[y] = x;
+    }
+    else if (ranks[y] < ranks[x])
+    {
+        parent[x] = y;
+    }
+    else
+    {
+        parent[x] = y;
+        ranks[x]++;
+    }
+}
+
+int main()
+{
+    makeSet(10);
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        int x, y;
+        cin >> x >> y;
+        Union(x, y);
+    }
+    // If 2 and 3 belongs to the same component or not
+    if (findPar(2) != findPar(3))
+    {
+        cout << "Different Component" << endl;
+    }
+    else
+    {
+        cout << "Same Component" << endl;
+    }
+    return 0;
+}
